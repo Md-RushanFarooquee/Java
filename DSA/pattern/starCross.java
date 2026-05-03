7f09d8424705cc6cffb7929446c5e54e533eb707
Rushan <rushanfarooquee@gmail.com>
2026-05-03 19:03:31 +0530

    Add files via upload

---
+++ DSA/pattern/starCross.java
@@ -0,0 +1,33 @@
+/*  *     *
+     *   *
+       *
+     *   *
+    *      *
+*/
+
+import java.util.Scanner;
+
+public class starCross {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n (odd only): ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+
+            for(int j = 1; j <=n; j++){
+
+                if(j == i || j == n - i + 1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
