b5b3318b6f73a433067bd69850a579615ea0cd5b
Rushan <rushanfarooquee@gmail.com>
2026-05-09 14:20:25 +0530

    Add files via upload

---
+++ LeetCode/SearchInsertPositionBINARYSEARCHmethod.java
@@ -0,0 +1,14 @@
+public class SearchInsertPositionBINARYSEARCHmethod {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+        int index = -1;
+        while(low<=high){
+            int mid = (low+high) / 2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return low;
+    }
+}
