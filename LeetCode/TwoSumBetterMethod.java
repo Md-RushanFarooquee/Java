4af6af9208aebdf4507ba63c714646dc86f23709
Rushan <rushanfarooquee@gmail.com>
2026-05-10 11:54:43 +0530

    Add files via upload

---
+++ LeetCode/TwoSumBetterMethod.java
@@ -0,0 +1,10 @@
+public class TwoSumBetterMethod {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 1; i < nums.length ; i++) {
+            for(int j = i;j < nums.length ; j++) {
+            if (nums[j-i] + nums[j] == target) return new int[]{j-i,j};
+            }
+        }
+        return new int[]{0,0};
+    }
+}
