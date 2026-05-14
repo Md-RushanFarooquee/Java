046c2af294d90c0210d70549deaec3d3353a113c
Rushan <rushanfarooquee@gmail.com>
2026-05-14 13:26:43 +0530

    Add files via upload

---
+++ LeetCode/ArrangingCoinsMATH.java
@@ -0,0 +1,18 @@
+public class ArrangingCoinsMATH {
+    public int sqrt(long x) {
+        if(x == 0) return 0;
+        long low = 1,high = x;
+        while(low<=high){
+            long mid = low + (high - low)/2;
+            if(mid == x/mid) return (int)mid;
+            else if(mid > x / mid) high = mid - 1;
+            else low = mid + 1;
+        }
+        return (int)high;
+    }
+
+    public int arrangeCoins(int n) {
+        long m = (long)n;
+        return (sqrt(8*m+1) - 1) / 2;
+    }
+}
