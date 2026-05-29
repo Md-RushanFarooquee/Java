ff1c62eeb64a34b364b16f22af5b15c65974ef07
Rushan <rushanfarooquee@gmail.com>
2026-05-29 20:32:58 +0530

    Add files via upload

---
+++ LeetCode/CountTheNumberOfSpecialCharactersI.java
@@ -0,0 +1,18 @@
+public class CountTheNumberOfSpecialCharactersI {
+    public int numberOfSpecialChars(String word) {
+        boolean lower[] = new boolean[26];
+        boolean upper[] = new boolean[26];
+
+        for(int i = 0; i<word.length();i++){
+            char ch = word.charAt(i);
+            if(Character.isLowerCase(ch)) lower[ch - 'a'] = true;
+            else upper[ch-'A'] = true;
+        }
+        int count = 0;
+
+        for(int i = 0; i<26;i++){
+            if(lower[i] == true && upper[i] == true) count++;
+        }
+        return count;
+    }
+}
