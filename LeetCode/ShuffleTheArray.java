e7c2ed51de7fb45b6df287cb430539c815cfbbf4
Rushan <rushanfarooquee@gmail.com>
2026-06-01 19:12:44 +0530

    Add files via upload

---
+++ LeetCode/ShuffleTheArray.java
@@ -0,0 +1,10 @@
+public class ShuffleTheArray {
+    public int[] shuffle(int[] nums, int n) {
+        int ans[] = new int[nums.length];
+        for(int i = 0;i<n;i++){
+            ans[2 * i] = nums[i];
+            ans[2 * i + 1] = nums[i+n];
+        }
+        return ans;
+    }
+}
