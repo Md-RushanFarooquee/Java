ca8a691b2f905f0874d0252f3c3684b8149a9a6f
Rushan <rushanfarooquee@gmail.com>
2026-06-04 08:59:48 +0530

    Add files via upload

---
+++ LeetCode/LengthOfLastWord.java
@@ -0,0 +1,14 @@
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        int i = s.length() - 1;
+        int len = 0;
+        while(i>=0 && s.charAt(i) == ' '){
+            i--;
+        }
+        while(i>=0 && s.charAt(i) != ' '){
+            i--;
+            len++;
+        }
+        return len;
+    }
+}
