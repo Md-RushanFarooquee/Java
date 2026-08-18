ce1d5e9fefd4d037623392375fd89f823daa5584
Rushan <rushanfarooquee@gmail.com>
2026-08-18 15:22:08 +0530

    Add files via upload

---
+++ DSA/Strings/NonRepeatingCharacterII.java
@@ -0,0 +1,17 @@
+public class NonRepeatingCharacterII {
+    public static void main(String[] args) {
+        String s = "geeksforgeeks";
+        System.out.println(nonRepeatingChar(s));
+    }
+    public static char nonRepeatingChar(String s) {
+        char ans = '$';
+        int freq [] = new int[26];
+        for(int i = 0; i<s.length();i++){
+            freq[s.charAt(i) - 'a']++;
+        }
+        for(int i = 0; i<s.length();i++){
+            if(freq[s.charAt(i) - 'a'] == 1) return s.charAt(i);
+        }
+        return ans;
+    }
+}
